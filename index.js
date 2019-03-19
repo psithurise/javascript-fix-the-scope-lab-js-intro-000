11a63d2f7668d7508d0c8f01aaa3b801b228c429
Rose Stough <psithurise@programmer.net>
2019-03-19 16:11:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var animal = 'dog'
 const two = 2
 
 function myAnimal() {
+  var animal = 'dog'
   return animal
 }
 
@@ -14,8 +14,6 @@ function yourAnimal() {
 
 function add2(n) {
   return n + two
-
-  // Feel free to move things around
 }
 
 var funkyFunction = function() {
@@ -26,4 +24,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction.function()
+var theFunk = funkyFunction
